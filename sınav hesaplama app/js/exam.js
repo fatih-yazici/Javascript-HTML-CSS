5e71a7ad0bb48f940a161dc199f3f130a7593d0f
Fatih YAZICI <139749372+fatih-yazici@users.noreply.github.com>
2023-10-06 16:15:56 +0300

    Delete sınav hesaplama app directory

--- sınav hesaplama app/js/exam.js
+++
@@ -1,7 +0,0 @@
-function Hesapla(){
-    var v=Number(document.getElementById('vizeid').value);
-    let f=Number(document.getElementById('finalid').value);
-    var result= (v*0.4) + (f*0.6);
-    document.getElementById('sonucid').innerHTML='SONUC:' + result;
-    
-}
